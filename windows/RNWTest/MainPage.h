5cf52f4f0d88e57775d610eeac9e30a4d3b6ccc7
João Textor <joaotextor@gmail.com>
2023-09-29 09:16:45 -0300

    Adicionar arquivos de projeto.

---
+++ windows/RNWTest/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RNWTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RNWTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
